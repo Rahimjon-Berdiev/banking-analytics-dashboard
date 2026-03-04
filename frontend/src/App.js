ef01f0ea35e6d3e5a0a61f995367e19a6677766e
Rahimjon <rahimjon.berdiev@mail.ru>
2026-03-04 22:46:22 +0100

    feat: clean frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Banking Analytics Dashboard</h1>
+      <p>Welcome to your personal finance tracker!</p>
+    </div>
+  );
+}
+
+export default App;
